15c3689eb124247768b853c4dba02a1381268f38
matteocrippa <matteocrippa@users.noreply.github.com>
2019-08-14 12:32:52 +0200

    release 1.1.77

--- Example/Pods/Target Support Files/Proximiio/Proximiio-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Proximiio : NSObject
-@end
-@implementation PodsDummy_Proximiio
-@end
--- Example/Pods/Target Support Files/Proximiio/Proximiio-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double ProximiioVersionNumber;
-FOUNDATION_EXPORT const unsigned char ProximiioVersionString[];
-
