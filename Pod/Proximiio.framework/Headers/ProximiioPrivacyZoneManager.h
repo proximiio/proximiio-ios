7c5ca83b19844c5d24ec375567b4cc5098bc10b0
Matteo Crippa <matteocrippa@users.noreply.github.com>
2020-01-29 10:29:06 +0100

    extra missing header for lib version

---
+++ Pod/Proximiio.framework/Headers/ProximiioPrivacyZoneManager.h
@@ -0,0 +1,32 @@
+//
+//  ProximiioPrivacyZoneManager.h
+//  Proximiio
+//
+//  Created by Matej Drzik on 01/11/2018.
+//  Copyright © 2018 proximi.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ProximiioGeofence.h"
+#import "ProximiioLocation.h"
+#import "ProximiioPrivacyZone.h"
+
+@interface ProximiioPrivacyZoneManager : NSObject
+
+-(NSArray *)privacyZonesForLocation:(ProximiioLocation *)location;
+-(void)updateWithLocation:(ProximiioLocation *)location;
+-(void)exitAllPrivacyZones;
++(id)sharedManager;
+
+@property (weak) id delegate;
+@property (nonatomic, strong, readonly) NSArray *lastPrivacyZones;
+
+@end
+
+@protocol ProximiioPrivacyZoneManagerDelegate
+
+- (void)didEnterPrivacyZone:(ProximiioPrivacyZone *)privacyZone;
+- (void)didLeavePrivacyZone:(ProximiioPrivacyZone *)privacyZone;
+
+@end
+
